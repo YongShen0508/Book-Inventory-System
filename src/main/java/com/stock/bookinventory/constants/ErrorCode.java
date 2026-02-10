d2d0b51f07275cee36c6f35674a49862b87428f2
yong shen <yongshenng0508@gmail.com>
2026-02-10 23:02:58 +0800

    error code

---
+++ src/main/java/com/stock/bookinventory/constants/ErrorCode.java
@@ -0,0 +1,16 @@
+package com.stock.bookinventory.constants;
+
+public class ErrorCode {
+
+	public static final String RECORD_NOT_FOUND = "RECORD_NOT_FOUND";
+
+	public static final String INVALID_ITEM_REQUEST = "INVALID_ITEM_REQUEST";
+
+	public static final String INSUFFICIENT_STOCK = "INSUFFICIENT_STOCK";
+
+	public static final String ORDER_CREATION_FAILED = "ORDER_CREATION_FAILED";
+
+	public static final String NO_ORDER_UPDATED = "NO_ORDER_UPDATED";
+
+	public static final String ORDER_NOT_FOUND = "ORDER_NOT_FOUND";
+}
