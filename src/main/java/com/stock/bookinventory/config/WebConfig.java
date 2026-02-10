3daacef84de1239f98872b9353f25dd060f29ebf
yong shen <yongshenng0508@gmail.com>
2026-02-11 00:13:45 +0800

    config

--- src/main/java/com/stock/bookinventory/config/WebConfig.java
+++ src/main/java/com/stock/bookinventory/config/WebConfig.java
@@ -1,4 +1,19 @@
 package com.stock.bookinventory.config;
 
-public class WebConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.stock.bookinventory.logging.LoggingInterceptor;
+
+public class WebConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private LoggingInterceptor loggingInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor((HandlerInterceptor) loggingInterceptor);
+	}
 }
