f29e9b7c38b11de68662c2ec8be28721bf4ff9ff
yong shen <yongshenng0508@gmail.com>
2026-02-11 00:13:33 +0800

    async config

--- src/main/java/com/stock/bookinventory/config/AsyncConfig.java
+++ src/main/java/com/stock/bookinventory/config/AsyncConfig.java
@@ -1,4 +1,35 @@
 package com.stock.bookinventory.config;
 
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
 public class AsyncConfig {
+	@Bean(name = "notificationExecutor")
+	public ThreadPoolTaskExecutor notificationExecutor() {
+
+		ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+		threadPoolTaskExecutor.setCorePoolSize(3);
+		threadPoolTaskExecutor.setMaxPoolSize(4);
+		threadPoolTaskExecutor.setQueueCapacity(25);
+		threadPoolTaskExecutor.setThreadNamePrefix("NotificationExecutor-");
+		threadPoolTaskExecutor.initialize();
+
+		return threadPoolTaskExecutor;
+
+	}
+
+	@Bean(name = "taskExecutor")
+	public Executor taskExecutor() {
+
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(5);
+		executor.setMaxPoolSize(10);
+		executor.setQueueCapacity(100);
+		executor.setThreadNamePrefix("TaskExecutor-");
+		executor.initialize();
+		return executor;
+	}
+
 }
