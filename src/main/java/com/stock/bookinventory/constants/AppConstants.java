96e9b91fc2470d1917dee90cb8a714137c8e7525
yong shen <yongshenng0508@gmail.com>
2026-02-10 23:03:54 +0800

    app constants

--- src/main/java/com/stock/bookinventory/constants/AppConstants.java
+++ src/main/java/com/stock/bookinventory/constants/AppConstants.java
@@ -1,4 +1,20 @@
 package com.stock.bookinventory.constants;
 
 public class AppConstants {
+
+	public static final String PAGE_SIZE = "200";
+
+	public static final String OFFSET = "0";
+
+	public static final int EXPIRED_DATE = Math.toIntExact(System.currentTimeMillis() + 24 * 60 * 60 * 1000); // 24
+																												// hours
+	public static final int STOCK_LOW_THRESHOLD = 10;
+
+	public final int MAX_AUDIT_RETURN_VALUE_LENGTH = 2000;
+
+	public final int LOCK_TIMEOUT_SECONDS = 60;
+
+	public final int LOCK_MAX_RETRIES = 10;
+
+	public final int LOCK_RETRY_DELAY_MILLISECONDS = 100;
 }
