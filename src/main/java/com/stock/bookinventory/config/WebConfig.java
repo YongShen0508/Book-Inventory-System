c56b57799ad0b5b7d0c47b35f95401f9a0d2e330
yong shen <yongshenng0508@gmail.com>
2026-02-11 11:44:57 +0800

    update config

--- src/main/java/com/stock/bookinventory/config/WebConfig.java
+++ src/main/java/com/stock/bookinventory/config/WebConfig.java
@@ -1,19 +1,24 @@
 package com.stock.bookinventory.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.stock.bookinventory.logging.LoggingInterceptor;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	private final LoggingInterceptor loggingInterceptor;
+
 	@Autowired
-	private LoggingInterceptor loggingInterceptor;
+	public WebConfig(LoggingInterceptor loggingInterceptor) {
+		this.loggingInterceptor = loggingInterceptor;
+	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor((HandlerInterceptor) loggingInterceptor);
+		registry.addInterceptor(loggingInterceptor).addPathPatterns("/api/**");
 	}
 }
